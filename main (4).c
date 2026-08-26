eebc798a3efea187880852a442430a1965789dfa
Kerolainy Oliveira <kerolainydasilva5@gmail.com>
2026-08-26 11:47:49 -0300

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+ int main() {
+    float valor;
+    float total;
+    int quantidade, cupom;
+    int frete = 7;
+    
+    
+    printf("----PEDIDO - FAST FOOD---- ");
+    printf("Informe o valor do pedido? ");
+    scanf("%f", &valor);
+    printf("Informe a Quantidade do produto: ");
+    scanf("%d", &quantidade);
+    printf("Frete: ", frete);
+     
+    printf("Tem cupom? 1 = sim, 2 = não ");
+    scanf("%d", &cupom);
+    if (cupom == 1){
+    }
+    
+    return 0;
+    
+}
+
