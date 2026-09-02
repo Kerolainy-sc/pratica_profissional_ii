927a838034928a25bd2a0201af536833f3fa4fe3
Kerolainy Oliveira <kerolainydasilva5@gmail.com>
2026-09-02 12:02:34 -0300

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+     float n1, n2, n3;
+     float media;
+     char opcao;
+ 
+     printf("Lançar nota: s = sim / n = não");
+     scanf("%c", opcao);
+    
+     while (opcao == 's'){
+         printf("N1 = ");
+         scanf("%f", &n1);
+         printf("N2 = ");
+         media = (n1 + n2 ) / 2;
+         
+         printf("Continuar? s = sim / / n = não");
+         getchar();
+         scanf("%c", &opcao);
+
+         
+
+    
+        printf("Você gostaria de fazer o cadastro? 1 = sim / 2 =  não ");
+        scanf("%f", opcao);
+        
+   
