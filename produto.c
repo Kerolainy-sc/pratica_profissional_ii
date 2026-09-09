7656ff81e4595b7baa1899507dd58ac67c777d60
Kerolainy Oliveira <kerolainydasilva5@gmail.com>
2026-09-09 11:13:53 -0300

    Atividade feitas no dia 09/09

---
+++ produto.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    
+    float produto;
+    
+    printf("Digite o valor do produto: ");
+    scanf("%f", &produto);
+    
+    if (ptoduto<100 || produto>250 ){ 
+        printf("Valor invalido");
+      }else{ 
+        printf("Valor = %.2f", produto);
+          
+      }
+    return 0;
+    
+}
